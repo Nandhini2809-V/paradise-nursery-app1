dfa08db2efee8b9d69fe1e22ead768821609d1ed
Nandhini2809-V <nandhini28092006@gmail.com>
2026-07-19 20:16:32 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import './src/App.css';
-import AboutUs from './src/AboutUs';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CartSlice';
+import ProductList from './ProductList';
+import ShoppingCart from './ShoppingCart';
+import AboutUs from './AboutUs';
+import './App.css';
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer
+  }
+});
 
 function App() {
   return (
-    <div>
-      <AboutUs />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>🌿 Paradise Nursery</h1>
+        <AboutUs />
+        <ProductList />
+        <ShoppingCart />
+      </div>
+    </Provider>
   );
 }
 
