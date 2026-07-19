88c279e3d4c1417777993a403d3229651c761d35
Nandhini2809-V <nandhini28092006@gmail.com>
2026-07-19 19:53:58 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './src/App.css';
+import AboutUs from './src/AboutUs';
+
+function App() {
+  return (
+    <div>
+      <AboutUs />
+    </div>
+  );
+}
+
+export default App;
